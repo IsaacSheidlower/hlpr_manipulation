f2ec716a0b8ca8d99c7468d2358fa2126d3dcd5c
Isaac Sheidlower <55815140+IsaacSheidlower@users.noreply.github.com>
2021-09-17 12:24:24 -0400

    Update rl_actions.py

--- hlpr_manipulation_utils/src/hlpr_manipulation_utils/rl_actions.py
+++ hlpr_manipulation_utils/src/hlpr_manipulation_utils/rl_actions.py
@@ -22,7 +22,7 @@ from hlpr_manipulation_utils.manipulator import Gripper
 import hlpr_manipulation_utils.transformations as Transform
 from hlpr_manipulation_utils.msg import RLActionServerActionGoal, RLActionServerActionResult, RLActionServerActionFeedback
 from hlpr_manipulation_utils.msg import RLActionServerAction
-from geometry_msgs.msg import Pose, PoseStamped
+from geometry_msgs.msg import Pose, PoseStamped, Point
 
 class RlActionServerclass:
   def __init__(self):
@@ -30,9 +30,47 @@ class RlActionServerclass:
     _result = RLActionServerActionResult()
 
     self._name = 'rl_actions'
-    self.server = actionlib.SimpleActionServer('rl_actions', RLActionServerAction, self.execute, False)
+    self.server = actionlib.SimpleActionServer('rl_actions', RLActionServerAction, self.execute, auto_start=False)
     self.server.start()
 
+    self.collison_service = rospy.ServiceProxy('/check_state_validity', GetStateValidity)
+
+    self.arm = ArmMoveIt("j2s7s300_link_base")
+    self.grip = Gripper()
+
+
   def execute(self, goal):
-    
+    """
+      Given a message of type geometry_msgs/Pose , move arm to 
+      that goal with collision checking. The goal must be similar to 
+      the current position as this action server is meant for 
+      real time reinforcement learning. The orientation given
+      in the Pose msg is more or less ingored.
+
+      goal: geometry_msgs/Pose
+    """
+    r = rospy.Rate(.1)
+    success = True
     self.server.set_succeeded()
+
+    rospy.loginfo('%s: Checking collisions and executing at time %i' % 
+                  (self._name, goal.order, rospy.Time.now()))
+
+    goal_pose = np.array([goal.position.x, goal.position.y, goal.postion.z])
+    goal_joints = arm.get_IK(goal)
+
+    # check change in pose is small: 
+
+    # get current joints in sorted order
+    arm_curr_pose = self.arm.get_current_pose()
+    curr_joints = np.array([arm_curr_pose[x] for x in sorted(arm_curr_pose)])
+    curr_pose = np.array([self.arm.get_FK()[0].pose.position.x, self.arm.get_FK()[0].pose.position.y,
+                          self.arm.get_FK()[0].pose.position.z])
+
+    # check for current collision or goal collision:
+    curr_robot_state = arm.state_from_joints(arm_curr_pose)
+
+    eef_change = curr_pose - goal_pose
+    joint_change = curr_joints - goal_joints           
+
+    
